80e2dec8efc91f8162d15063078d9866cf500d90
otomomik <115955017+otomomik@users.noreply.github.com>
2023-04-07 20:20:33 +0900

    fish関連の修正 (#3)
    * fish関連の修正
    
    * formatt
    
    ---------
    
    Co-authored-by: otomomik <otomomik@otomomik.local>

--- bin/config/nvim/lua/plugins.lua
+++ bin/config/nvim/lua/plugins.lua
@@ -2,16 +2,16 @@ local fn = vim.fn
 
 local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-	PACKER_BOOTSTRAP = fn.system({
-		"git",
-		"clone",
-		"--depth",
-		"1",
-		"https://github.com/wbthomason/packer.nvim",
-		install_path,
-	})
-	print("Installing packer close and reopen Neovim...")
-	vim.cmd.packadd("packer.nvim")
+  PACKER_BOOTSTRAP = fn.system({
+    "git",
+    "clone",
+    "--depth",
+    "1",
+    "https://github.com/wbthomason/packer.nvim",
+    install_path,
+  })
+  print("Installing packer close and reopen Neovim...")
+  vim.cmd.packadd("packer.nvim")
 end
 
 vim.cmd([[
@@ -23,140 +23,140 @@ vim.cmd([[
 
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
-	return
+  return
 end
 
 packer.init({
-	display = {
-		open_fn = function()
-			return require("packer.util").float({ border = "rounded" })
-		end,
-	},
+  display = {
+    open_fn = function()
+      return require("packer.util").float({ border = "rounded" })
+    end,
+  },
 })
 
 return packer.startup(function(use)
-	use("wbthomason/packer.nvim")
-	use("cocopon/iceberg.vim")
-	use({ "ibhagwan/fzf-lua", requires = { "nvim-tree/nvim-web-devicons" } })
-	use({
-		"ojroques/nvim-hardline",
-		config = function()
-			require("hardline").setup({})
-		end,
-	})
-	use({
-		"lewis6991/gitsigns.nvim",
-		config = function()
-			require("gitsigns").setup({})
-		end,
-	})
-	use("lambdalisue/fern.vim")
-	use("neovim/nvim-lspconfig")
-	use({
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup({})
-		end,
-	})
-	use({
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			local opt = {
-				capabilities = require("cmp_nvim_lsp").default_capabilities(),
-			}
-			require("mason-lspconfig").setup_handlers({
-				function(server)
-					require("lspconfig")[server].setup(opt)
-				end,
-			})
-		end,
-	})
-	use({
-		"hrsh7th/nvim-cmp",
-		config = function()
-			local cmp = require("cmp")
-			cmp.setup({
-				snippet = {
-					expand = function(args)
-						vim.fn["vsnip#anonymous"](args.body)
-					end,
-				},
-				sources = {
-					{ name = "nvim_lsp" },
-				},
-				mapping = cmp.mapping.preset.insert({
-					["<C-k>"] = cmp.mapping.select_prev_item(),
-					["<C-j>"] = cmp.mapping.select_next_item(),
-					["<CR>"] = cmp.mapping.confirm({ select = true }),
-				}),
-				experimental = {
-					ghost_text = true,
-				},
-			})
-			cmp.setup.filetype("gitcommit", {
-				sources = cmp.config.sources({
-					{ name = "cmp_git" },
-				}, {
-					{ name = "buffer" },
-				}),
-			})
-			cmp.setup.cmdline({ "/", "?" }, {
-				mapping = cmp.mapping.preset.cmdline(),
-				sources = {
-					{ name = "buffer" },
-				},
-			})
-			cmp.setup.cmdline(":", {
-				mapping = cmp.mapping.preset.cmdline(),
-				sources = cmp.config.sources({
-					{ name = "path" },
-				}, {
-					{ name = "cmdline" },
-				}),
-			})
-		end,
-	})
-	use("hrsh7th/cmp-nvim-lsp")
-	use("hrsh7th/cmp-path")
-	use("hrsh7th/cmp-cmdline")
-	use("hrsh7th/vim-vsnip")
-	use("lambdalisue/fern-git-status.vim")
-	use("lambdalisue/fern-renderer-devicons.vim")
-	use("lambdalisue/fern-renderer-nerdfont.vim")
-	use("ryanoasis/vim-devicons")
-	use("lambdalisue/nerdfont.vim")
-	use({
-		"jose-elias-alvarez/null-ls.nvim",
-		requires = { "nvim-lua/plenary.nvim" },
-		config = function()
-			local status, null_ls = pcall(require, "null-ls")
-			if not status then
-				return
-			end
-			null_ls.setup({
-				sources = {
-					null_ls.builtins.diagnostics.eslint.with({
-						prefer_local = "node_modules/.bin",
-					}),
-					null_ls.builtins.formatting.prettier,
-					null_ls.builtins.formatting.stylua,
-					null_ls.builtins.formatting.tidy,
-					null_ls.builtins.diagnostics.stylelint,
-					null_ls.builtins.formatting.fish_indent,
-					null_ls.builtins.formatting.shellharden,
-				},
-				debug = false,
-			})
-		end,
-	})
-	use({
-		"norcalli/nvim-colorizer.lua",
-		config = function()
-			require("colorizer").setup()
-		end,
-	})
+  use("wbthomason/packer.nvim")
+  use("cocopon/iceberg.vim")
+  use({ "ibhagwan/fzf-lua", requires = { "nvim-tree/nvim-web-devicons" } })
+  use({
+    "ojroques/nvim-hardline",
+    config = function()
+      require("hardline").setup({})
+    end,
+  })
+  use({
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("gitsigns").setup({})
+    end,
+  })
+  use("lambdalisue/fern.vim")
+  use("neovim/nvim-lspconfig")
+  use({
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup({})
+    end,
+  })
+  use({
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      local opt = {
+        capabilities = require("cmp_nvim_lsp").default_capabilities(),
+      }
+      require("mason-lspconfig").setup_handlers({
+        function(server)
+          require("lspconfig")[server].setup(opt)
+        end,
+      })
+    end,
+  })
+  use({
+    "hrsh7th/nvim-cmp",
+    config = function()
+      local cmp = require("cmp")
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            vim.fn["vsnip#anonymous"](args.body)
+          end,
+        },
+        sources = {
+          { name = "nvim_lsp" },
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-k>"] = cmp.mapping.select_prev_item(),
+          ["<C-j>"] = cmp.mapping.select_next_item(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        }),
+        experimental = {
+          ghost_text = true,
+        },
+      })
+      cmp.setup.filetype("gitcommit", {
+        sources = cmp.config.sources({
+          { name = "cmp_git" },
+        }, {
+          { name = "buffer" },
+        }),
+      })
+      cmp.setup.cmdline({ "/", "?" }, {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = "buffer" },
+        },
+      })
+      cmp.setup.cmdline(":", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = "path" },
+        }, {
+          { name = "cmdline" },
+        }),
+      })
+    end,
+  })
+  use("hrsh7th/cmp-nvim-lsp")
+  use("hrsh7th/cmp-path")
+  use("hrsh7th/cmp-cmdline")
+  use("hrsh7th/vim-vsnip")
+  use("lambdalisue/fern-git-status.vim")
+  use("lambdalisue/fern-renderer-devicons.vim")
+  use("lambdalisue/fern-renderer-nerdfont.vim")
+  use("ryanoasis/vim-devicons")
+  use("lambdalisue/nerdfont.vim")
+  use({
+    "jose-elias-alvarez/null-ls.nvim",
+    requires = { "nvim-lua/plenary.nvim" },
+    config = function()
+      local status, null_ls = pcall(require, "null-ls")
+      if not status then
+        return
+      end
+      null_ls.setup({
+        sources = {
+          null_ls.builtins.diagnostics.eslint.with({
+            prefer_local = "node_modules/.bin",
+          }),
+          null_ls.builtins.formatting.prettier,
+          null_ls.builtins.formatting.stylua,
+          null_ls.builtins.formatting.tidy,
+          null_ls.builtins.diagnostics.stylelint,
+          null_ls.builtins.formatting.fish_indent,
+          null_ls.builtins.formatting.shellharden,
+        },
+        debug = false,
+      })
+    end,
+  })
+  use({
+    "norcalli/nvim-colorizer.lua",
+    config = function()
+      require("colorizer").setup()
+    end,
+  })
 
-	if PACKER_BOOTSTRAP then
-		require("packer").sync()
-	end
+  if PACKER_BOOTSTRAP then
+    require("packer").sync()
+  end
 end)
