78c7eff5edde71e3633f25f3895b199c9875fbd0
otomomik <otomomik@otomomik.local>
2023-04-07 20:20:10 +0900

    formatt

--- bin/config/fish/config.fish
+++ bin/config/fish/config.fish
@@ -22,10 +22,10 @@ alias mv="mv -i"
 
 # homebrew
 if test -e /etc/os-release
-  # WSL
+    # WSL
 else
-  # MacOS
-  eval (/opt/homebrew/bin/brew shellenv)
+    # MacOS
+    eval (/opt/homebrew/bin/brew shellenv)
 end
 
 source ~/.asdf/asdf.fish
@@ -42,21 +42,21 @@ set PATH $PATH $HOME/bin
 
 # act
 if test -e /etc/os-release
-  # WSL
-  set PATH $PATH $HOME/bin
+    # WSL
+    set PATH $PATH $HOME/bin
 else
-  # MacOS
+    # MacOS
 end
 
 # ReactNative
 if test -e /etc/os-release
-  # WSL
-  set JAVA_HOME /usr/lib/jvm/java-11-openjdk-amd64
-  set ANDROID_HOME $HOME/Android
-  set ANDROID_SDK_ROOT $ANDROID_HOME
-  set PATH $PATH $JAVA_HOME/bin $ANDROID_HOME/emulator $ANDROID_HOME/tools $ANDROID_HOME/tools/bin $ANDROID_HOME/platform-tools
-  alias react-native-start="yarn start --host 127.0.0.1"
-  alias react-native-android="yarn android --variant=debug --deviceId emulator-5554"
+    # WSL
+    set JAVA_HOME /usr/lib/jvm/java-11-openjdk-amd64
+    set ANDROID_HOME $HOME/Android
+    set ANDROID_SDK_ROOT $ANDROID_HOME
+    set PATH $PATH $JAVA_HOME/bin $ANDROID_HOME/emulator $ANDROID_HOME/tools $ANDROID_HOME/tools/bin $ANDROID_HOME/platform-tools
+    alias react-native-start="yarn start --host 127.0.0.1"
+    alias react-native-android="yarn android --variant=debug --deviceId emulator-5554"
 end
 
 # bundle
@@ -71,20 +71,20 @@ alias ws='cd ~/workspace'
 
 # fisher
 if not test -e ~/.config/fish/fisher/install.fish
-  echo "fisher install"
-  mkdir ~/.config/fish/fisher
-  curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish > ~/.config/fish/fisher/install.fish
-  source ~/.config/fish/fisher/install.fish
+    echo "fisher install"
+    mkdir ~/.config/fish/fisher
+    curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish >~/.config/fish/fisher/install.fish
+    source ~/.config/fish/fisher/install.fish
 end
 if not test -e ~/.config/fish/fisher/jorgebucaran-fisher
-  source ~/.config/fish/fisher/install.fish
-  fisher install jorgebucaran/fisher
-  touch ~/.config/fish/fisher/jorgebucaran-fisher
+    source ~/.config/fish/fisher/install.fish
+    fisher install jorgebucaran/fisher
+    touch ~/.config/fish/fisher/jorgebucaran-fisher
 end
 if not test -e ~/.config/fish/fisher/oh-my-fish-theme-bobthefish
-  source ~/.config/fish/fisher/install.fish
-  fisher install oh-my-fish/theme-bobthefish
-  touch ~/.config/fish/fisher/oh-my-fish-theme-bobthefish
+    source ~/.config/fish/fisher/install.fish
+    fisher install oh-my-fish/theme-bobthefish
+    touch ~/.config/fish/fisher/oh-my-fish-theme-bobthefish
 end
 
 cd ~
