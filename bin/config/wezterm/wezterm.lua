3882dc9eaa1e5d616c9cd6d45e669c9910aab82c
otomomik <developer@otomomik.com>
2023-04-19 21:03:00 +0900

    null-lsの追加

--- bin/config/wezterm/wezterm.lua
+++ bin/config/wezterm/wezterm.lua
@@ -7,4 +7,23 @@ config.color_scheme = "iceberg-dark"
 -- WSL
 -- config.default_prog = { 'wsl.exe' }
 
+config.keys = {
+  {
+    key = '|',
+    mods = 'CTRL|SHIFT|ALT',
+    action = wezterm.action.SplitPane {
+      direction = 'Right',
+      size = { Percent = 50 },
+    },
+  },
+  {
+    key = '_',
+    mods = 'CTRL|SHIFT|ALT',
+    action = wezterm.action.SplitVertical {
+      domain = 'CurrentPaneDomain'
+    },
+  },
+}
+
+
 return config
