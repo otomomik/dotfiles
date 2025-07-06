39cb75a27da693f3be1665488d6a4be9066fa0e4
otomomik <otomomik@pluie-lab.com>
2025-07-06 14:16:02 +0900

    feat: Claudeコマンドのセットアップを追加

--- link.sh
+++ link.sh
@@ -9,3 +9,7 @@ ln -snfv $(pwd)/config ~/.config
 # tmux
 ln -snfv $(pwd)/.tmux.conf ~/
 ln -snfv $(pwd)/tmux ~/.tmux
+
+# claude
+mkdir -p ~/.claude/commands
+ln -snfv $(pwd)/claude-commands/*.md ~/.claude/commands/
