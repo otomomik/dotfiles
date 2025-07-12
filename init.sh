60e8e40bc697dab40922fdad62b4266378303b97
otomomik <otomomik@pluie-lab.com>
2025-07-12 09:13:14 +0900

    feat: 開発環境セットアップの改善
    - Pythonのバージョンをlatestに更新
    - brewパッケージをカテゴリごとに整理
    - 開発ツール（ripgrep, fd, orbstack）を追加
    - ブラウザ（vivaldi, google-chrome）を追加

--- init.sh
+++ init.sh
@@ -8,6 +8,11 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zdharma-continuum/history-search-multi-word.git ~/.oh-my-zsh/custom/plugins/history-search-multi-word
 
-brew install --cask arc wezterm 1password raycast slack alt-tab claude windsurf
-brew install wget tmux gh mise lazysql uv
+# utility
+brew install --cask 1password raycast slack alt-tab claude
+# development
+brew install --cask wezterm orbstack
+brew install wget tmux gh mise lazysql ripgrep fd
 wget -O ./tmux/iceberg_minimal.tmux.conf https://raw.githubusercontent.com/gkeep/iceberg-dark/master/.tmux/iceberg_minimal.tmux.conf
+# browser
+rew install --cask arc vivaldi google-chrome
